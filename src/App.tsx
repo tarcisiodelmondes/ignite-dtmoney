8d379b1bd76e89e5f3f1a1049a87e7956213c2ca
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-09 10:18:01 -0300

    se aplicado esse commit, adicionara um modal na pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+
+import Modal from "react-modal";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
 
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <Dashboard />
     </>
   );
