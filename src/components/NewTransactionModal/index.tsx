985766705a06ed737f1b02b263f003025a12e123
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-10 15:19:56 -0300

    Modal finalizado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,14 @@
 import Modal from "react-modal";
 
+import { RadioBox, Container, TransactionTypeContainer } from "./styles";
+
+import closeImg from "../../assets/close.svg";
+
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -9,9 +18,81 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState<"deposit" | "withdraw">("deposit");
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  async function handleCreateNewTransaction(e: FormEvent) {
+    e.preventDefault();
+
+    const data = { type, title, value: Number(value), category };
+
+    await api.post("/transactions", data);
+  }
+
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container onSubmit={handleCreateNewTransaction}>
+        <button
+          type="button"
+          className="react-modal-close"
+          onClick={onRequestClose}
+        >
+          <img src={closeImg} alt="Fechar modal" />
+        </button>
+
+        <h2>Cadastrar transação</h2>
+
+        <input
+          type="text"
+          placeholder="Titulo"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            isActive={type === "deposit"}
+            activeColor={"green"}
+            type="button"
+            onClick={() => setType("deposit")}
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            isActive={type === "withdraw"}
+            activeColor={"red"}
+            type="button"
+            onClick={() => setType("withdraw")}
+          >
+            <img src={outcomeImg} alt="Saida" />
+            <span>Entrada</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input
+          type="text"
+          placeholder="Categoria"
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+        />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,100 @@
 import styled from "styled-components";
+import { darken, transparentize } from "polished";
 
-export const Container = styled.div``;
+export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2.25rem;
+  }
+
+  input {
+    width: 100%;
+
+    background-color: #e7e9ee;
+    border-radius: 0.24rem;
+    border: 1px solid #d7d7d7;
+
+    padding: 0 1.5rem;
+
+    height: 4rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    margin-top: 1.5rem;
+
+    background-color: var(--green);
+    border-radius: 0.24rem;
+
+    color: var(--shape);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 64px;
+    width: 100%;
+
+    font-weight: 600;
+    transition: filter 0.3s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
+
+const colors = {
+  green: "#33CC95",
+  red: "#E52E4D",
+};
+
+export const RadioBox = styled.button`
+  background-color: ${({ isActive, activeColor }: RadioBoxProps) =>
+    isActive ? transparentize(0.9, colors[activeColor]) : "transparent"};
+
+  border: 1.5px solid #d7d7d7;
+  border-radius: 5px;
+
+  height: 64px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.3s;
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    color: var(--text-title);
+    font-size: 1rem;
+    margin-left: 1rem;
+  }
+
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
+`;
